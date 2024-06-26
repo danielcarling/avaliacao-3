24a66719ca3b0a34fd5b6dc476e3e3af93b57fb9
Daniel Carling <117768647+danielcarling@users.noreply.github.com>
2024-06-26 16:53:47 -0400

    update

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,7 +2,7 @@
   <div class="wrapper">
     <h1>Visão de Tarefas</h1>
     <div class="container">
-      <v-container class="username">{{ userStore.user.displayName
+      <v-container>{{ userStore.user.displayName
         }}</v-container>
       <v-btn v-if="!!userStore.isAuthenticated"
         @click="userStore.signOut">Logout</v-btn>
@@ -32,9 +32,5 @@ const userStore = useUserStore();
   justify-content: flex-end;
   padding: 10px;
   align-items: center;
-}
-
-.username {
-  /* width: 500px; */
 }
 </style>
